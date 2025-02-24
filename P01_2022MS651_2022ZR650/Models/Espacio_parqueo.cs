3180c5b3648b12caf6f6a9379408aa85403e4496
Ever Zamora <ever.zamora@catolica.edu.sv>
2025-02-24 17:37:46 -0600

    Modelo: Reserva y esapcio_parqueos

---
+++ P01_2022MS651_2022ZR650/Models/Espacio_parqueo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P01_2022MS651_2022ZR650.Models
+{
+    public class Espacio_parqueo
+    {
+        [Key]
+        public int id {  get; set; }
+        [Required]
+        public int sucursal_id { get; set; }
+        public int numero {  get; set; }
+        public string ubicacion { get; set; }
+        public decimal costo_por_hora { get; set; }
+        public string estado {  get; set; }
+
+    }
+}
---
+++ P01_2022MS651_2022ZR650/Models/Reserva.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022MS651_2022ZR650.Models
+{
+    public class Reserva
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public int usuario_id { get; set; }
+        [Required]
+        public int espacio_id {  get; set; }
+        public DateTime fecha { get; set; }
+        public int cantidad_horas { get; set; }
+    }
+}
