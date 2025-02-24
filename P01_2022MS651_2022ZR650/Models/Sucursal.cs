34c67bdd55b4bc45f6c6649f48887d4daab32a86
Maicol Monge <maicol.monge@catolica.edu.sv>
2025-02-24 17:37:40 -0600

    Modelos usuario y sucursal

---
+++ P01_2022MS651_2022ZR650/Models/Sucursal.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022MS651_2022ZR650.Models
+{
+    public class Sucursal
+    {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string? telefono { get; set; }
+
+        [Required]
+        public int administrador_id { get; set; }
+        public int num_espacios { get; set; }
+    }
+}
--- P01_2022MS651_2022ZR650/Models/parkingContext.cs
+++ P01_2022MS651_2022ZR650/Models/parkingContext.cs
@@ -7,6 +7,8 @@ namespace P01_2022MS651_2022ZR650.Models
         public parkingContext(DbContextOptions<parkingContext> options) : base(options)
         {
         }
-    
+        public DbSet<Usuario> usuarios { get; set; }
+        public DbSet<Sucursal> sucursales { get; set; }
+
     }
 }
